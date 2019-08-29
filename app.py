8c3ed26c2c94504f8a6277cd0badfbb30be3d53f
Phuvisa <51754479+Phuvisa@users.noreply.github.com>
2019-08-29 14:27:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,11 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    user = decoded["events"][0]['replyToken']
-    userText = decoded["events"][0]['message']['text']
-    #sendText(user,userText)
+    #user = decoded["events"][0]['replyToken']
+    user = decoded['originaldetectintentrequest']['payload']['data']['replytoken']
+    #userText = decoded["events"][0]['message']['text']
+    Usertext = decoded['queryresult']['intent']['displayname']
+    sendText(user,userText)
     if (userText == 'กินข้าวหรือยัง') :
         sendText(user,'กินข้าวแล้วจร้า')
     elif (userText == 'ไปเที่ยวกันไหม') :
